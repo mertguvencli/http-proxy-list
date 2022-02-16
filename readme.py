8a700ae34cd325df0189498959c16e1523c70965
Mert Güvençli <guvenclimert@gmail.com>
2022-02-16 14:23:20 +0300

    readme.py updated

--- readme.py
+++ readme.py
@@ -2,41 +2,44 @@ from sources import SOURCES
 import json
 
 
+README = """
+# Free HTTP Proxy List 🌍
+
+[![Hourly Update](https://github.com/mertguvencli/http-proxy-list/actions/workflows/main.yml/badge.svg?branch=main)](https://github.com/mertguvencli/http-proxy-list/actions/workflows/main.yml)
+
+It is a lightweight project that hourly scrapes lots of free-proxy sites, validates if it works, and serves a clean proxy list.
+
+## Usage
+
+Click the file that you want and copy the URL.
+
+|File|Content|
+|----|-------|
+|[data.txt](/proxy-list/data.txt)|`ip_address:port` combined (seperated new line)|
+|[data.json](/proxy-list/data.json)|`ip, port`|
+|[data-with-geolocation.json](/proxy-list/data-with-geolocation.json)|`ip, port, geolocation`|
+
+## Sources
+
+{{SOURCES}}
+
+## Sample Proxies With Geolocation Info
+
+|#|Ip|Port|Country|City|Internet Service Provider|
+|-|--|----|-------|----|-------------------------|
+{{PROXY_LIST}}
+
+
+## Contributing
+
+Contributions are welcome, and they are greatly appreciated! Every
+little bit helps, and credit will always be given.
+
+"""  # noqa
+
+
 def update_readme():
-    README = """
-    # Free HTTP Proxy List 🌍
-
-    [![Hourly Update](https://github.com/mertguvencli/http-proxy-list/actions/workflows/main.yml/badge.svg?branch=main)](https://github.com/mertguvencli/http-proxy-list/actions/workflows/main.yml)
-
-    It is a lightweight project that hourly scrapes lots of free-proxy sites, validates if it works, and serves a clean proxy list.
-
-    ## Usage
-
-    Click the file that you want and copy the URL.
-
-    |File|Content|
-    |----|-------|
-    |[data.txt](/proxy-list/data.txt)|`ip_address:port` combined (seperated new line)|
-    |[data.json](/proxy-list/data.json)|`ip, port`|
-    |[data-with-geolocation.json](/proxy-list/data-with-geolocation.json)|`ip, port, geolocation`|
-
-    ## Sources
-
-    {{SOURCES}}
-
-    ## Sample Proxies With Geolocation Info
-
-    |#|Ip|Port|Country|City|Internet Service Provider|
-    |-|--|----|-------|----|-------------------------|
-    {{PROXY_LIST}}
-
-
-    ## Contributing
-
-    Contributions are welcome, and they are greatly appreciated! Every
-    little bit helps, and credit will always be given.
-
-    """
+    global README
 
     SOURCES_MD = ''
     for config in SOURCES:
